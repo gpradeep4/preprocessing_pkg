66d50d3a28c48284566dd06a599a992f5c622dc0
guttikonda pradeep <gpradeep86@outlook.com>
2021-09-28 15:14:58 +0530

    Python Preprocessing for NLP

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open('README.md', 'r') as file:
+	long_description = file.reade()
+
+
+setuptools.setup(
+	name = 'preprocess_gp',
+	version = '0.0.1',
+	author = 'pradeep',
+	author_email = 'gpradeep86@outlook.com',
+	description = 'This is preprocessing package',
+	long_description = long_description,
+	long_description_content_type = 'text/markdown'
+	packages = setuptools.find_packages(),
+	classifiers = [
+	'Programming Language :: Python :: 3',
+	'License :: OSI Aproved :: MIT License',
+	"Operating System :: OS Independent"],
+	python_requires = '>=3.5'
+	)
